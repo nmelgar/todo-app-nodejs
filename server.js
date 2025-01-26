8066537370b221bca1b67a785e64278485e0e049
nmelgar <nmelgar16013@gmail.com>
2025-01-25 23:40:54 -0600

    Update readme.md file. Update comments.

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
+// import libraries
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// create instance of express app
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// connect to mongodb using uri from .env file 
 mongoose.connect(process.env.MONGODB_URI);
 
+// schema for items
 const itemsSchema = {
   name: String
 };
 
+// create mongoose model
 const Item = mongoose.model('Item', itemsSchema, 'todo-items');
 
+// handle get request to the root url
 app.get('/', async (req, res) => {
   try {
     const foundItems = await Item.find({});
@@ -26,6 +32,7 @@ app.get('/', async (req, res) => {
   }
 });
 
+// handle post request to add a new item
 app.post('/', async (req, res) => {
   const itemName = req.body.newItem;
   const item = new Item({ name: itemName });
@@ -37,6 +44,7 @@ app.post('/', async (req, res) => {
   }
 });
 
+// handle post request to delete an item
 app.post('/delete', async (req, res) => {
   const checkedItemId = req.body.checkbox;
   try {
@@ -47,6 +55,7 @@ app.post('/delete', async (req, res) => {
   }
 });
 
+// start server on port 3000
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
