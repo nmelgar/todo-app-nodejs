8a4a0aa001461c9c6f0f6d4e763280d1a5992da2
nmelgar <nmelgar16013@gmail.com>
2025-01-25 18:33:12 -0600

    Add .env for connection to db.

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model('Item', itemsSchema, 'todo-items');
+
+app.get('/', async (req, res) => {
+  try {
+    const foundItems = await Item.find({});
+    res.render('list', { newListItems: foundItems });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post('/', async (req, res) => {
+  const itemName = req.body.newItem;
+  const item = new Item({ name: itemName });
+  try {
+    await item.save();
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post('/delete', async (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  try {
+    await Item.findByIdAndDelete(checkedItemId);
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
